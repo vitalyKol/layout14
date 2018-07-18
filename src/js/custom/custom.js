9e1b645e5ba1919987ccb93254ab9a0cfc45b2bc
Vitaly <vental-kvo@yandex.ru>
2018-07-19 00:16:49 +0300

    comlited slider and posts

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,7 +1,17 @@
 $(function() {
 
 
+$("#js-testimonials").slick({
+  dots: false,
+  arrows: false
+});
 
+$(".testimonials__controls_left").click(function(){
+	$("#js-testimonials").slick("slickPrev");
+});
 
+$(".testimonials__controls_right").click(function(){
+	$("#js-testimonials").slick("slickNext");
+});
 
 });
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 ===========================*/
 
 //= ../../bower_components/jquery/dist/jquery.min.js
-
+//= ../../bower_components/slick-carousel/slick/slick.min.js
 
 /* Custom
 ===========================*/
