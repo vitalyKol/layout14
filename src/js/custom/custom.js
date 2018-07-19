a8a81617a547e9de986fd18348ac10fc5b9ffb6b
Vitaly <vental-kvo@yandex.ru>
2018-07-19 18:43:06 +0300

    all layout is complited

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,17 +1,27 @@
 $(function() {
 
 
-$("#js-testimonials").slick({
-  dots: false,
-  arrows: false
-});
+	$("#js-testimonials").slick({
+	  dots: false,
+	  arrows: false
+	});
 
-$(".testimonials__controls_left").click(function(){
-	$("#js-testimonials").slick("slickPrev");
-});
+	$(".testimonials__controls_left").click(function(){
+		$("#js-testimonials").slick("slickPrev");
+	});
 
-$(".testimonials__controls_right").click(function(){
-	$("#js-testimonials").slick("slickNext");
-});
+	$(".testimonials__controls_right").click(function(){
+		$("#js-testimonials").slick("slickNext");
+	});
+
+
+	$(".footer__top-arrow").click(function(){
+
+		var blockOffset = $("#js-head").offset().top;
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 1000);
+	});
 
 });
